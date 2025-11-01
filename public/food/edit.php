f9c1dbdb2fcf50dc15b071251b9a5fdea771de8f
Sasita Hobi <sasita.hobi@heig-vd.ch>
2025-11-01 21:19:01 +0100

    Update edit.php
    Fonctionnalité

--- public/food/edit.php
+++ public/food/edit.php
@@ -13,3 +13,25 @@ if (!isset($_GET['id']) || !ctype_digit($_GET['id'])) {
 }
 
 $id = (int) $_GET['id'];
+
+// récupération des données actuelles de l'aliment
+$food = $foodManager->getFood($id);
+
+if (!$food) {
+    echo "<p>L'aliment est introuvable</p>";
+    exit();
+}
+
+// mettre à jour lorsque le formulaire est soumis
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $updatedData = [
+        "name" => $_POST["name"] ?? "",
+        "shop" => $_POST["shop"] ?? "",
+        "qty" => $_POST["qty"] ?? 0,
+        "unit" => $_POST["unit"] ?? "",
+        "spot" => $_POST["spot"] ?? "",
+        "peremption" => $_POST["peremption"] ?? "",
+        "notes" => $_POST["notes"] ?? ","
+    ];
+}
+?>
