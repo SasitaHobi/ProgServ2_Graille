0e9967ff4aa1087f5eae2680c009fe9b193ee61d
PlumsPie <sarah.furrer@hes-so.ch>
2025-11-03 14:06:29 +0100

    Traductions pour page edit

--- public/assets/translations.php
+++ public/assets/translations.php
@@ -18,7 +18,11 @@ $text_translations=[
         'createTitle'=> 'Create new food item | Graille',
         'createH1'=> 'Create new food item ',
         'createSuccess'=> 'Food item successfully added !',
-        'createError'=>'The form contains errors :'
+        'createError'=>'The form contains errors :',
+        'editTitle'=>'Edit food item | Graille',
+        'editH1'=>'Edit food item',
+        'editBack'=>'Back to homepage',
+        'editSave'=>'Save changes'
     ],
     'fr'=> [
         'homeTitle'=> 'Page d\'accueil | Graille',
@@ -38,7 +42,11 @@ $text_translations=[
         'createTitle'=> 'Créer un nouvel aliment | Graille',
         'createH1'=> 'Créer un nouvel aliment',
         'createSuccess'=> 'L\'aliment a été ajouté avec succès !',
-        'createError'=>'Le formulaire contient des erreurs :'
+        'createError'=>'Le formulaire contient des erreurs :',
+        'editTitle'=>'Modifier un aliment | Graille',
+        'editH1'=>'Modifier un aliment',
+        'editBack'=>'Retour à l\'accueil',
+        'editSave'=>'Enregistrer les modification'
 
     ]
     ];
@@ -48,14 +56,16 @@ $text_translations=[
         'createShop'=> 'Shop name must be at leat two characters.',
         'createQty'=> 'Quantity is required.',
         'createUnit'=> 'Unit is required.',
-        'createSpot'=> 'Spot is required.'
+        'createSpot'=> 'Spot is required.',
+        'editFood'=>'Food item cannot be found.'
     ],
     'fr'=> [
         'createName'=> 'Le nom de l\'aliment doit contenir au moins 2 caractères.',
         'createShop'=> 'Le nom du magasin doit contenir au moins 2 caractères.',
         'createQty'=> 'La quantité doit être un nombre positif.',
         'createUnit'=> 'L\'unité est obligatoire.',
-        'createSpot'=> 'L\'emplacement est obligatoire.'
+        'createSpot'=> 'L\'emplacement est obligatoire.',
+        'editFood'=>'L\'aliment est introuvable'
     ]
     ];
 
