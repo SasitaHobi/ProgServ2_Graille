b11e644baf912e62a3d6cdcb704b4d2f1e82249d
Sasita Hobi <sasita.hobi@heig-vd.ch>
2025-11-03 13:30:03 +0100

    adding some html

--- public/food/edit.php
+++ public/food/edit.php
@@ -35,3 +35,59 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     ];
 }
 ?>
+
+// partie html
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modifier un aliment</title>
+</head>
+
+<body>
+    <main class="container">
+        <h1>Modifier un aliment</h1>
+        <p><a href="index.php">Retour à l'accueil</a></p>
+
+        <form method="POST">
+            <label for="name">Nom de l'aliment</label>
+            <input type="text" name="name" id="id" value="<?= htmlspecialchars($food['name']) ?>" required />
+
+            <label for="shop">Magasin :</label>
+            <input type="text" name="shop" id="shop" value="<?= htmlspecialchars($food['shop']) ?>" required />
+
+            <label for="qty">Quantité :</label>
+            <input type="number" name="qty" id="qty" value="<? htmlspecialchars($food['qty']) ?>" min="0" required />
+
+            <label for="unit">Unité :</label>
+            <select name="unit" id="unit">
+                <?php foreach (Food::UNITS as $key => $value) { ?>
+                    <option value="<?= $key ?>" <?= $food['unit'] == $key ? 'selected' : '' ?>><?= $value ?></option>
+                <?php } ?>
+            </select>
+
+            <label for="spot">Emplacement :</label>
+            <select name="spot" id="spot">
+                <?php foreach (Food::SPOTS as $key => $value) { ?>
+                    <option value="<?= $key ?>" <?= $food['spot'] == $key ? 'selected' : '' ?>><?= $value ?></option>
+                <?php } ?>
+            </select>
+
+            <label for="peremption">Date de péremption :</label>
+            <input type="date" name="peremption" id="peremption" value="<?= htmlspecialchars($food['peremption']) ?>" />
+
+            <label for="notes">Notes :</label>
+            <textarea name="notes" id="notes" rows="4" cols="50"><?= htmlspecialchars($food['notes']) ?></textarea>
+
+            <div style="margin-top: 1rem; display: flex; gap: .5em;">
+                <button type="submit">Enregistrer les modification</button>
+                <a href="view.php?id=<?= htmlspecialchars($food['id']) ?>"><button type="button" class="annulation">Annuler</button></a>
+            </div>
+        </form>
+    </main>
+</body>
+
+</html>
